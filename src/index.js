6df4ce86f0af7f2b33f0359d814341ff2e13a261
Aubin Olivrie <aubin.olivrie@smice.com>
2024-05-04 12:53:05 +0200

    [FIX] No console.log & show which service is running

--- src/index.js
+++ src/index.js
@@ -104,5 +104,5 @@ app.post('/chat', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.info(`[IA] Server is running on port ${PORT}`);
 });
